ff6815097cda37c9cccfedd108535343a638ddf7
migue <migue@LAPTOP-2BRLB9DG.mshome.net>
2023-05-21 17:07:58 +0100

    login e registo

--- Projeto/src/main/java/com/example/demo/MainController.java
+++ Projeto/src/main/java/com/example/demo/MainController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,23 +14,61 @@ public class MainController {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private ServiceRepository serviceRepository;
+
+	@Autowired
+	private WorkerRepository workerRepository;
+
+	@Autowired
+	private HistoryRepository historyRepository;
+
 	@GetMapping(path = "/")
 	public String getAllDeps(Model model) {
 		model.addAttribute("ListUsers", userRepository.findAll());
 		return "index";
 	}
 
-	@PostMapping("/saveUser")
-	public String saveUser(@ModelAttribute("newUser") User user) {
-		userRepository.save(user);
-		return "redirect:/";
-	}
-
-	@GetMapping("/register")
+	@GetMapping("/Register")
 	public String register(Model model) {
 		User user = new User();
 		model.addAttribute("newUser", user);
 		return "register";
+	}
+
+	@PostMapping("/saveUser")
+	public String saveUser(@ModelAttribute("newUser") User user) {
+		List<User> users = userRepository.findByEmail(user.getEmail());
+		if (users.size() != 0) {
+			System.out.println("User ja existe");
+		}
+		userRepository.save(user);
+		return "redirect:/";
+	}
+
+	@GetMapping("/Login")
+	public String login(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		return "login";
+	}
+
+	@PostMapping("/validateUser")
+	public String validateUser(@ModelAttribute("user") User user) {
+		List<User> users = userRepository.findByEmail(user.getEmail());
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getPassword().equals(user.getPassword())) {
+				return "redirect:/";
+			}
+		}
+		return "login";
+	}
+
+	@GetMapping("/NewService")
+	public String newService(Model model) {
+		Service service = new Service();
+		model.addAttribute(service);
+		return "newService";
 	}
 
 }
--- Projeto/src/main/java/com/example/demo/UserRepository.java
+++ Projeto/src/main/java/com/example/demo/UserRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+	List<User> findByEmail(String email);
 
 }
